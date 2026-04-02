01ba183c841d569baa7f8300a95d1bfcac80dfc3
Abhi <abhinashsia@gmail.com>
2026-04-02 20:40:59 +0530

    A litle fixes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,17 @@
-// src/routes/auth.routes.js
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser } from "../controllers/auth.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllers/auth.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/logout").post(verifyJWT, logoutUser); // protected — need to be logged in to logout
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh").post(refreshAccessToken);
 
 export default router;
